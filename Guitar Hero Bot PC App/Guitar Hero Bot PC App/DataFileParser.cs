07033f86c6d6c74038dcc4eee239c430a8599313
Matt Dissinger <matt.dissinger@gmail.com>
2015-03-06 09:55:42 -0600

    Add start of solution for the PC app portion of Guitar Hero Bot

---
+++ Guitar Hero Bot PC App/Guitar Hero Bot PC App/DataFileParser.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guitar_Hero_Bot_PC_App
+{
+    class DataFileParser
+    {
+        public DataFileParser(string sFileName)
+        {
+            m_sFileName = sFileName;
+        }
+
+        public bool ParseDataFile()
+        {
+            try
+            {
+                var oFile = new System.IO.StreamReader(m_sFileName);
+
+                string sLine;
+                while ((sLine = oFile.ReadLine()) != null)
+                {
+                    if (ParseDataLine(sLine))
+                    {
+                        // Status error set
+                        return false;
+                    }
+                }
+            }
+            catch (System.IO.IOException)
+            {
+                // File not found
+                m_sStatus = "File not found.";
+                return false;
+            }
+
+            return true;
+        }
+
+        private bool ParseDataLine(string sLine)
+        {
+            if (String.IsNullOrWhiteSpace(sLine) || sLine.StartsWith("#"))
+            {
+                // Comment line
+                return true;
+            }
+            else if (sLine.StartsWith("fps:"))
+            {
+                if (m_nFramesPerSecond > 0)
+                {
+                    m_sStatus = "Second fps line found.";
+                    return false;
+                }
+
+                string[] parts = sLine.Split(':');
+                if (parts.Count() != 2)
+                {
+                    m_sStatus = "Invalid fps line encountered.";
+                    return false;
+                }
+
+                int nFrames;
+                if (!Int32.TryParse(parts[1], out nFrames))
+                {
+                    m_sStatus = "Unable to parse frames per second.";
+                    return false;
+                }
+
+                if (nFrames <= 0 || nFrames > 1000)
+                {
+                    m_sStatus = "Frames out of bounds (1,1000)";
+                    return false;
+                }
+
+                m_nFramesPerSecond = nFrames;
+                m_dMillisecondsPerFrame = 1000.0 / m_nFramesPerSecond;
+            }
+            else
+            {
+                if (m_nFramesPerSecond == 0)
+                {
+                    m_sStatus = "Encountered command line before fps line";
+                    return false;
+                }
+
+                string[] parts = sLine.Split(',');
+                if (parts.Count() != 2)
+                {
+                    m_sStatus = "Invalid line found: " + sLine;
+                    return false;
+                }
+
+                int nFrames;
+                if (!Int32.TryParse(parts[0], out nFrames))
+                {
+                    m_sStatus = "Unable to parse frame delay on line: " + sLine;
+                    return false;
+                }
+
+                if (nFrames < 0)
+                {
+                    m_sStatus = "Frame delay count must be 0 or greater.";
+                    return false;
+                }
+
+                if (parts[1].Count() != 6)
+                {
+                    m_sStatus = "Incorrect format for button inputs: " + sLine;
+                    return false;
+                }
+
+                var BotCommand = new GuitarBotCommand();
+                BotCommand.m_nMillisecondDelay = (int)((double)nFrames * m_dMillisecondsPerFrame);
+
+                if (parts[1].ElementAt(0) == 'G')
+                {
+                    BotCommand.m_bGreenActive = true;
+                }
+                if (parts[1].ElementAt(1) == 'R')
+                {
+                    BotCommand.m_bRedActive = true;
+                }
+                if (parts[1].ElementAt(2) == 'Y')
+                {
+                    BotCommand.m_bYellowActive = true;
+                }
+                if (parts[1].ElementAt(3) == 'B')
+                {
+                    BotCommand.m_bBlueActive = true;
+                }
+                if (parts[1].ElementAt(4) == 'O')
+                {
+                    BotCommand.m_bOrangeActive = true;
+                }
+                if (parts[1].ElementAt(5) == 'S')
+                {
+                    BotCommand.m_bDoStrum = true;
+                }
+
+                m_qBotCommands.Enqueue(BotCommand);
+            }
+
+            return true;
+        }
+
+        private string m_sFileName;
+        private string m_sStatus;
+        private int m_nFramesPerSecond;
+        private double m_dMillisecondsPerFrame;
+        private Queue<GuitarBotCommand> m_qBotCommands;
+    }
+}
---
+++ Guitar Hero Bot PC App/Guitar Hero Bot PC App/GuitarBotCommand.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guitar_Hero_Bot_PC_App
+{
+    class GuitarBotCommand
+    {
+        public GuitarBotCommand()
+        {
+            m_nMillisecondDelay = 0;
+            m_bGreenActive = false;
+            m_bRedActive = false;
+            m_bYellowActive = false;
+            m_bBlueActive = false;
+            m_bOrangeActive = false;
+            m_bDoStrum = false;
+        }
+
+        // How many milliseconds to delay before executing this command
+        public int m_nMillisecondDelay;
+
+        public bool m_bGreenActive;
+        public bool m_bRedActive;
+        public bool m_bYellowActive;
+        public bool m_bBlueActive;
+        public bool m_bOrangeActive;
+        public bool m_bDoStrum;
+    }
+}
